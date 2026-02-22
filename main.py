d4395ddaeb8f8be5166c1bdccc6b5303915cacdd
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-02-22 18:50:20 +0530

    Adding pagination in main py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def home():
     search = request.args.get("search", "")
     category = request.args.get("category", "")
     company = request.args.get("company", "")
+    page = int(request.args.get("page", 1))
 
     params = {}
 
@@ -23,8 +24,15 @@ def home():
     response = requests.get(API_URL, params=params)
     data = response.json()
 
-    jobs = data.get("jobs", [])
-    return render_template("index.html", jobs=jobs, search=search, category = category, company = company)
+    all_jobs = data.get("jobs", [])
+    per_page = 10
+    start = (page - 1) * per_page
+    end = start + per_page
+
+    jobs = all_jobs[start:end]
+    total_pages = len(all_jobs) // per_page + (1 if len(all_jobs)% per_page else 0 )
+
+    return render_template("index.html", jobs=jobs, search=search, category = category, company = company, page=page, total_pages=total_pages)
 
 if __name__ == "__main__":
     app.run(debug=True)
